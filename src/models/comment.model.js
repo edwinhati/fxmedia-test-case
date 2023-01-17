b419e0031e3e4015fa5ef24727e72e32472d9a0a
Edwin Hati <52129921+edwinhati@users.noreply.github.com>
2023-01-17 07:08:57 +0000

    Fix post API

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     name: {
         type: String,
+        required: true
     },
     comment: {
         type: String,
+        required: true
     },
     date: {
         type: Date,
