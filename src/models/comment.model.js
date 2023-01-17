9d133887c15358411687a0000c311a3d28489315
Edwin Hati <52129921+edwinhati@users.noreply.github.com>
2023-01-17 06:07:33 +0000

    working on comment api

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -3,11 +3,9 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     name: {
         type: String,
-        required: true,
     },
     comment: {
         type: String,
-        required: true,
     },
     date: {
         type: Date,
